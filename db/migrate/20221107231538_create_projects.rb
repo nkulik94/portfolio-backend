8c6fed9aa6980dc38d7eb6b7c633ad90308b2ad1
Naftali Kulik <nkulik1994@gmail.com>
2022-11-07 18:16:45 -0500

    Created model for projects

---
+++ db/migrate/20221107231538_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :description
+      t.string :technologies
+      t.string :demo
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end

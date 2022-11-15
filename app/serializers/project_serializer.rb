248a3c09e2d7b0f4573c539d081f337dddbafd1c
Naftali Kulik <nkulik1994@gmail.com>
2022-11-15 11:34:28 -0500

    Update serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,3 +1,11 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :short_description, :github, :link, :description, :demo_embed_link
+  attributes :id, :title, :short_description, :github, :link, :description, :embed_links, :technologies_list
+
+  def embed_links
+    self.object.demo_embed_link.split('|')
+  end
+
+  def technologies_list
+    self.object.technologies.split('|')
+  end
 end
